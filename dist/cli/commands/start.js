4f792154863cca6ededd1f1f6e8e27d88bd33105
Richard Foster <richard@madetech.co.uk>
2017-03-19 20:18:36 +0000

    Temporarily remove piping

--- dist/cli/commands/start.js
+++ dist/cli/commands/start.js
@@ -1,15 +1,12 @@
 'use strict';
 
-var pipe = require('piping');
 var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
 var config = require('../../lib/config');
 
 function start() {
-  if (process.env.NODE_ENV !== 'development' || pipe()) {
-    global.webpackIsomorphic = new WebpackIsomorphicTools(config.webpackIsomorphicTools).server(process.cwd(), function () {
-      return require(config.serverPath);
-    });
-  }
+  global.webpackIsomorphic = new WebpackIsomorphicTools(config.webpackIsomorphicTools).server(process.cwd(), function () {
+    return require(config.serverPath);
+  });
 }
 
 module.exports = start;
--- src/cli/commands/start.js
+++ src/cli/commands/start.js
@@ -2,10 +2,8 @@ const WebpackIsomorphicTools = require('webpack-isomorphic-tools')
 const config = require('../../lib/config')
 
 function start () {
-  if (process.env.NODE_ENV !== 'development' || require('piping')(config.rootPath)) {
-    global.webpackIsomorphic = new WebpackIsomorphicTools(config.webpackIsomorphicTools)
-      .server(process.cwd(), () => require(config.serverPath))
-  }
+  global.webpackIsomorphic = new WebpackIsomorphicTools(config.webpackIsomorphicTools)
+    .server(process.cwd(), () => require(config.serverPath))
 }
 
 module.exports = start
